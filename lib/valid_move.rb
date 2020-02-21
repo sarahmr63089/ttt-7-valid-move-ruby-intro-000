60d9733b345e572a09268d86cdfd2c9b3d6c38bc
Sarah Rose <sarahrose63089@gmail.com>
2020-02-21 20:31:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
     if  (index.between?(0, 8)) && (!position_taken?(board, index))
         return true
-    elsif board[index] == "X" || "O" #also this isn't it?
+    elsif board[index] == "X" || "O"
       return false
     else index > 8
       return false
